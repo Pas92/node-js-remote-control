7154323c36aec9a9a8cf3c62ab6184231ff36186
Pas92 <pavelburdavitsyn92@gmail.com>
2023-01-24 00:17:42 +0300

    fix: fix screenshots

--- src/screen-reader.ts
+++ src/screen-reader.ts
@@ -3,12 +3,53 @@ import Jimp from 'jimp/es';
 
 export const sendScreen = async (): Promise<string> => {
   const mousePosition = await mouse.getPosition();
-  const region = new Region(
-    mousePosition.x - 100 >= 0 ? mousePosition.x - 100 : 0,
-    mousePosition.y - 100 >= 0 ? mousePosition.y - 100 : 0,
-    200,
-    200
-  );
+  const screenWidth = await screen.width();
+  const screenHeight = await screen.height();
+
+  let regionX = 0;
+  let regionY = 0;
+
+  switch (true) {
+    case mousePosition.x - 100 <= 0 &&
+      mousePosition.y - 100 >= screenHeight - 200:
+      regionX = 0;
+      regionY = screenHeight - 200;
+      break;
+    case mousePosition.x - 100 <= 0 && mousePosition.y >= screenHeight - 200:
+      regionX = 0;
+      regionY = screenHeight - 200;
+      break;
+    case mousePosition.x - 100 <= 0 &&
+      mousePosition.y - 100 <= screenHeight - 200:
+      regionX = 0;
+      regionY = mousePosition.y;
+      break;
+    case mousePosition.x <= screenWidth - 200 &&
+      mousePosition.y >= screenHeight - 200:
+      regionX = mousePosition.x;
+      regionY = screenHeight - 200;
+      break;
+    case mousePosition.x >= screenWidth - 200 &&
+      mousePosition.y <= screenHeight - 200:
+      regionX = screenWidth - 200;
+      regionY = mousePosition.y;
+      break;
+    case mousePosition.x >= screenWidth - 200 &&
+      mousePosition.y >= screenHeight - 200:
+      regionX = screenWidth - 200;
+      regionY = screenHeight - 200;
+      break;
+    case mousePosition.x - 100 >= screenWidth - 200 &&
+      mousePosition.y - 100 <= screenHeight - 200:
+      regionX = screenWidth - 200;
+      regionY = mousePosition.y;
+      break;
+    default:
+      regionX = mousePosition.x;
+      regionY = mousePosition.y;
+  }
+
+  const region = new Region(regionX, regionY, 200, 200);
 
   const img = await screen.grabRegion(region);
   const rgbImage = await img.toRGB();
